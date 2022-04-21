aa8721ec8b61b3af1b5db777e0bb16a09e408d71
tylerasa <sarpongasaresylvester@gmail.com>
2022-04-21 14:55:27 +0000

    +mapbox-gl

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Map } from "react-map-gl";
+import { AmbientLight, PointLight, LightingEffect } from "@deck.gl/core";
+import { HexagonLayer } from "@deck.gl/aggregation-layers";
+import DeckGL from '@deck.gl/react';
+const DATA_URL =
+  "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv"; // eslint-disable-line
+
+const ambientLight = new AmbientLight({
+  color: [255, 255, 255],
+  intensity: 1.0
+});
+
+const pointLight1 = new PointLight({
+  color: [255, 255, 255],
+  intensity: 0.8,
+  position: [-0.144528, 49.739968, 80000]
+});
+
+const pointLight2 = new PointLight({
+  color: [255, 255, 255],
+  intensity: 0.8,
+  position: [-3.807751, 54.104682, 8000]
+});
+
+const lightingEffect = new LightingEffect({
+  ambientLight,
+  pointLight1,
+  pointLight2
+});
+
+const material = {
+  ambient: 0.64,
+  diffuse: 0.6,
+  shininess: 32,
+  specularColor: [51, 51, 51]
+};
+
+const INITIAL_VIEW_STATE = {
+  longitude: -1.415727,
+  latitude: 52.232395,
+  zoom: 6.6,
+  minZoom: 5,
+  maxZoom: 15,
+  pitch: 40.5,
+  bearing: -27
+};
+
+const MAP_STYLE =
+  "https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json";
+
+export const colorRange = [
+  [1, 152, 189],
+  [73, 227, 206],
+  [216, 254, 181],
+  [254, 237, 177],
+  [254, 173, 84],
+  [209, 55, 78]
+];
+
+function getTooltip({ object }) {
+  if (!object) {
+    return null;
+  }
+  const lat = object.position[1];
+  const lng = object.position[0];
+  const count = object.points.length;
+
+  return `\
+    latitude: ${Number.isFinite(lat) ? lat.toFixed(6) : ""}
+    longitude: ${Number.isFinite(lng) ? lng.toFixed(6) : ""}
+    ${count} Accidents`;
+}
 
 function App() {
+  const [data, setData] = useState();
+
+  var mapStyle = MAP_STYLE;
+  var radius = 1000;
+  var upperPercentile = 100;
+  var coverage = 1;
+  const layers = [
+    new HexagonLayer({
+      id: "heatmap",
+      colorRange,
+      coverage,
+      data,
+      elevationRange: [0, 3000],
+      elevationScale: data && data.length ? 50 : 0,
+      extruded: true,
+      getPosition: d => d,
+      pickable: true,
+      radius,
+      upperPercentile,
+      material,
+
+      transitions: {
+        elevationScale: 3000
+      }
+    })
+  ];
+
+  useEffect(() => {
+    axios.get(DATA_URL).then(res => {
+      console.log(res);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DeckGL
+        layers={layers}
+        effects={[lightingEffect]}
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        getTooltip={getTooltip}
+      >
+        <Map reuseMaps mapStyle={mapStyle} preventStyleDiffing={true} />
+      </DeckGL>
     </div>
   );
 }
