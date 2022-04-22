cb94f1c02fc4874abf13276e8ecc208c40c7e689
tylerasa <sarpongasaresylvester@gmail.com>
2022-04-22 13:34:23 +0000

    +getting country

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
 import TrendMap from "./components/TrendMap";
 import axios from "axios";
 import { useEffect } from "react";
+import Geocode from "react-geocode";
 
 function App() {
+  console.log(process.env.REACT_APP_GOOGLE_MAPS_API);
+  Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API);
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+  function showPosition(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    console.log(lat, lng);
+    Geocode.fromLatLng(lat, lng).then(
+      response => {
+        const firstAddress = response.results[0].address_components;
+
+        console.log(firstAddress[firstAddress.length- 1].long_name);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+    // map.setCenter(new google.maps.LatLng(lat, lng));
+  }
   useEffect(() => {
     var config = {
       method: "get",
-      url: "https://api.twitter.com/1.1/trends/place.json?id=23424824",
+      url: `${process.env.REACT_APP_BASE_URL}/countries`,
       headers: {
-        Authorization:
-          "Bearer AAAAAAAAAAAAAAAAAAAAAH2lbAEAAAAAoTLWB0FqzBbqeueksaqNec6EfCg%3DbTjzLjgOc2nEK2qCaj95exv9l7suRHnxfrMbY2Q6d4UcE379zD",
-        "access-control-allow-origin": "*"
+        "Access-Control-Allow-Origin": "*"
       }
     };
 
     axios(config)
       .then(function(response) {
-        console.log(JSON.stringify(response.data));
+        // console.log(JSON.stringify(response.data));
       })
       .catch(function(error) {
         console.log(error);
       });
   }, []);
+
+  useEffect(() => {
+    getLocation();
+  }, []);
   return (
     <div>
-      <TrendMap />
+      <p>hello</p>
+      {/* <TrendMap /> */}
     </div>
   );
 }
